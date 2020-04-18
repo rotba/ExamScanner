542bb0a38691bb26086579ed6b4870014857909d
rotemha <rotemhas@post.bgu.ac.il>
2020-04-18 08:12:40 +0300

    תשתית טסטים

--- app/src/androidTest/java/com/example/examscanner/communication/ImageProcessorTest.java
+++ app/src/androidTest/java/com/example/examscanner/communication/ImageProcessorTest.java
@@ -1,7 +1,11 @@
 package com.example.examscanner.communication;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -17,7 +21,11 @@ import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
@@ -25,9 +33,28 @@ import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 @RunWith(AndroidJUnit4.class)
 public class ImageProcessorTest {
 
-    @Test
-    public void splitImageTestZeroChunks(){
+    Mat pdfTest;
+
+    @Before
+    public void setUp() {
         OpenCVLoader.initDebug();
+        pdfTest = loadFromResource("test_jpeg_diagonal1");
+        // pdfTest = loadFromAssets("test_jpeg_diagonal1");
+    }
+
+    private Bitmap bitmapFromMat(Mat mat){
+        Bitmap bm = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mat, bm);
+        return bm;
+    }
+
+    private Mat matFromBitmap(Bitmap bm){
+        Mat mat = new Mat();
+        Utils.bitmapToMat(bm, mat);
+        return mat;
+    }
+
+    private Mat loadFromResource(String test_jpeg_diagonal1) {
 
         Mat img = null;
         try {
@@ -37,9 +64,27 @@ public class ImageProcessorTest {
             e.printStackTrace();
         }
 
-     //   Mat img = Imgcodecs.imread("unnamed");
-        Bitmap bm = Bitmap.createBitmap(img.cols(), img.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(img, bm);
+        return img;
+    }
+
+    private Bitmap loadFromAssets(String test_jpeg_diagonal1) {
+
+        Bitmap bitmap = null;
+        try {
+            InputStream ims = getApplicationContext().getAssets().open("test_jpg_1.jpg");
+            bitmap = BitmapFactory.decodeStream(ims);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+    @Test
+    public void splitImageTestZeroChunks() throws IOException {
+
+        Bitmap bm = bitmapFromMat(pdfTest);
         ArrayList<Bitmap> chunks = ImageProcessor.splitImage(bm, 0);
         assert bm.equals(chunks.get(0));
     }
