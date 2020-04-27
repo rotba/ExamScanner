67895522bcf3f73dabcacb4d19c5976907d82118
Rotem <rotba@post.bgu.ac.il>
2020-04-27 16:36:23 +0300

    TEST: should require the user creating a exam to be its manager PASS

--- test/test.js
+++ test/test.js
@@ -30,8 +30,8 @@ function adminApp() {
   return firebase.initializeAdminApp({ databaseName }).database();
 }
 
-function createExam(){
-	return true;
+function createExam(manager){
+	return {manager:manager};
 };
 
 function createVersion(examId){
@@ -42,12 +42,12 @@ function createQuestion(versionId){
 	return {versionId:versionId.toString()};
 };
 
-async function createExamContext(db, id){
-  await db.ref(`exams/${id}`).set(createExam());
+async function createExamContext(db, id, manager){
+  await db.ref(`exams/${id}`).set(createExam(manager));
 }
 
-async function createVersionContext(db, id, examId){
-  createExamContext(db,examId);
+async function createVersionContext(db, id, examId, manager){
+  createExamContext(db,examId, manager);
   await db.ref(`versions/${id}`).set(createVersion(id,examId));
 }
 
@@ -81,7 +81,7 @@ after(async () => {
 describe("versions validate rules", () => {
   it("should have FK to exams", async () => {
     const alice = authedApp({ uid: "alice" });
-    createExamContext(alice, 1);
+    createExamContext(alice, 1, "alice");
     await firebase.assertSucceeds(alice.ref("versions/1").set(createVersion(1)));
     await firebase.assertFails(alice.ref("versions/2").set(createVersion(2)));
   });
@@ -90,10 +90,36 @@ describe("versions validate rules", () => {
 describe("questions validate rules", () => {
   it("should have FK to versions", async () => {
     const alice = authedApp({ uid: "alice" });
-    createVersionContext(alice, 1,1);
+    createVersionContext(alice, 1,1, "alice");
     await firebase.assertSucceeds(alice.ref("questions/1").set(createQuestion(1)));
     await firebase.assertFails(alice.ref("questions/2").set(createQuestion(2)));
   });
 });
+
+describe("exam creation", () => {
+  it("should require the user creating a exam to be its manager", async () => {
+    const alice = authedApp({ uid: "alice" });
+
+    // should not be able to create exam managed by another user
+    await firebase.assertFails(alice.ref("exams/1").set(createExam("bob")));
+    // should not be able to create room with no owner
+    await firebase.assertFails(
+      alice.ref("exams/1").set({ graders: { alice: true } })
+    );
+    // alice should be allowed to create a room she owns
+    await firebase.assertSucceeds(
+      alice.ref("exams/1").set(createExam("alice"))
+    );
+  });
+});
+
+// describe("exam manager write rules", () => {
+//   it("should seal the exam write permissions to the exam graders except the exam manager", async () => {
+//     const alice = authedApp({ uid: "alice" });
+//     createVersionContext(alice, 1,1);
+//     await firebase.assertSucceeds(alice.ref("questions/1").set(createQuestion(1)));
+//     await firebase.assertFails(alice.ref("questions/2").set(createQuestion(2)));
+//   });
+// });
 
 
