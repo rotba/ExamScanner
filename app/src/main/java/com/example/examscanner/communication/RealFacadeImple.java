104dad763894c8f66353bcd4cbe9e8cf555f3838
rotemha <rotemhas@post.bgu.ac.il>
2020-06-22 19:40:37 +0300

    fixed bug remote and local exams in home not correlated

--- app/src/main/java/com/example/examscanner/communication/RealFacadeImple.java
+++ app/src/main/java/com/example/examscanner/communication/RealFacadeImple.java
@@ -386,15 +386,24 @@ public class RealFacadeImple implements CommunicationFacade {
     @Override
     public ExamEntityInterface[] getExams() {
         List<Exam> examEntities = db.getExamDao().getAll();
+        List<Exam> onlyRemoteExams = new ArrayList<>();
         List<com.example.examscanner.persistence.remote.entities.Exam> remoteExams = new ArrayList<>();
         remoteDb.getExams().blockingSubscribe(res -> remoteExams.addAll(res));
         for (com.example.examscanner.persistence.remote.entities.Exam re :
                 remoteExams) {
             if (!examExists(examEntities, re)) {
                 importRemoteExam(re);
-                examEntities.add(db.getExamDao().getByCoursenameSemeseterTermYear(re.courseName, re.semester, re.term, re.year));
+                Exam importedExam = db.getExamDao().getByCoursenameSemeseterTermYear(re.courseName, re.semester, re.term, re.year);
+                examEntities.add(importedExam);
+                onlyRemoteExams.add(importedExam);
+            }
+            else{
+                onlyRemoteExams.add(localExamEntity(examEntities, re));
             }
         }
+        List<Exam> toRemove = new ArrayList<>(examEntities);
+        toRemove.removeAll(onlyRemoteExams);
+        examEntities.removeAll(toRemove);
         ExamEntityInterface[] ans = new ExamEntityInterface[examEntities.size()];
         for (int i = 0; i < ans.length; i++) {
             ans[i] = examEntity2EntityInterface(examEntities.get(i));
@@ -469,6 +478,15 @@ public class RealFacadeImple implements CommunicationFacade {
                         s.getTerm() == re.term
         ).toArray();
         return arr.length == 1;
+    }
+
+    private Exam localExamEntity(List<Exam> examEntities, com.example.examscanner.persistence.remote.entities.Exam re) {
+        Object[] arr = examEntities.stream().filter(s ->
+                s.getCourseName().equals(re.courseName) &&
+                        s.getSemester() == re.semester &&
+                        s.getTerm() == re.term
+        ).toArray();
+        return (Exam)arr[0];
     }
 
     @Override
